e5be7682876cb0cd4d54e473add1029a91dd3a5d
borjamh <bmartinh@gmail.com>
2019-09-11 10:30:18 +0200

    Update Entrada.java

--- src/Entrada.java
+++ src/Entrada.java
@@ -2,5 +2,6 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.println("Ejemplo de nueva linea de código");
         System.out.println("Linea de código realizada por un externo");
+        System.out.println("linea escrita por el usuario principal");
     }
 }
